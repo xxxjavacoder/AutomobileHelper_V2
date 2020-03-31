3336acb43587466477776dd3d300345d094a8b85
xxxjavacoder <baraban9099@gmail.com>
2020-03-31 15:25:37 +0300

    add K - coef of answer and add test number 2!!!

--- test_2/script.js
+++ test_2/script.js
@@ -8,19 +8,19 @@ function check() {
 
     let arr_of_k = [0.0, 0.0, 0.0, 0.0, 0.0];
 
-    let  answers = ["Возможно отсуцтвие искры в свечах зажигания!", "Проблема может быть в катушках и их проводке!",
-         "Проверьте клемы аккамулятора , они могли окислится!", "Комутатор может быть неисправен!",
-         "Проверьте проводку бензонасоса и его работоспособность!"];
+    let  answers = ["Проблема скорее всего в датчике холостого хода и ему сопутствующих!", "Проблема может быть в свечах , катушках и их проводке!",
+        "Неисправность может быть в инжекторе и топливной системе!", "Возможно качество топлива в баке сомнительное, разбавте хорошим бензином!",
+        "Скорее всего в двигатель попала вода или конденсат, попробуйте прогазовать авто и дать ему прогрется!"];
 
 
     let r1 = document.getElementsByName("r1");
 
     if (getValueFromRadio(r1) === "0") {
 
-        arr_of_k[0] += 0.5;
-        arr_of_k[1] += 0.3;
-        arr_of_k[2] += 0.5;
-        arr_of_k[3] += 0.3;
+        arr_of_k[0] += 0.6;
+        arr_of_k[1] += 0.1;
+        arr_of_k[2] += 0.1;
+        arr_of_k[3] += 0.1;
         arr_of_k[4] += 0.1;
 
         cards[0].style.backgroundColor = '#CD5C5C';
@@ -31,10 +31,10 @@ function check() {
 
     if (getValueFromRadio(r2) === "0") {
 
-        arr_of_k[0] += 0.1;
-        arr_of_k[1] += 0.2;
-        arr_of_k[2] += 0.2;
-        arr_of_k[3] += 0.1;
+        arr_of_k[0] += 0.2;
+        arr_of_k[1] += 0.7;
+        arr_of_k[2] += 0.3;
+        arr_of_k[3] += 0.2;
         arr_of_k[4] += 0.0;
 
         cards[1].style.backgroundColor = '#CD5C5C';
@@ -42,13 +42,13 @@ function check() {
 
     let r3 = document.getElementsByName("r3");
 
-    if (getValueFromRadio(r3) === "0") {
+    if (getValueFromRadio(r3) === "1") {
 
-        arr_of_k[0] += 0.1;
-        arr_of_k[1] += 0.2;
+        arr_of_k[0] += 0.0;
+        arr_of_k[1] += 0.0;
         arr_of_k[2] += 0.2;
-        arr_of_k[3] += 0.3;
-        arr_of_k[4] += 0.1;
+        arr_of_k[3] += 0.1;
+        arr_of_k[4] += 0.0;
 
 
         cards[2].style.backgroundColor = '#CD5C5C';
@@ -59,10 +59,10 @@ function check() {
     if (getValueFromRadio(r4) === "0") {
 
         arr_of_k[0] += 0.0;
-        arr_of_k[1] += 0.0;
-        arr_of_k[2] += 0.0;
-        arr_of_k[3] += 0.2;
-        arr_of_k[4] += 0.3;
+        arr_of_k[1] += 0.1;
+        arr_of_k[2] += 0.3;
+        arr_of_k[3] += 0.5;
+        arr_of_k[4] += 0.0;
 
         cards[3].style.backgroundColor = '#CD5C5C';
     }else cards[3].style.backgroundColor = '#90EE90';
@@ -71,11 +71,11 @@ function check() {
 
     if (getValueFromRadio(r5) === "0") {
 
-        arr_of_k[0] += 0.0;
-        arr_of_k[1] += 0.0;
+        arr_of_k[0] += 0.2;
+        arr_of_k[1] += 0.1;
         arr_of_k[2] += 0.0;
         arr_of_k[3] += 0.0;
-        arr_of_k[4] += 0.4;
+        arr_of_k[4] += 0.0;
 
         cards[4].style.backgroundColor = '#CD5C5C';
     }else cards[4].style.backgroundColor = '#90EE90';
@@ -84,24 +84,24 @@ function check() {
 
     if (getValueFromRadio(r6) === "1") {
 
-        arr_of_k[0] += 0.3;
-        arr_of_k[1] += 0.1;
-        arr_of_k[2] += 0.1;
+        arr_of_k[0] += 0.0;
+        arr_of_k[1] += 0.0;
+        arr_of_k[2] += 0.0;
         arr_of_k[3] += 0.0;
-        arr_of_k[4] += 0.1;
+        arr_of_k[4] += 0.5;
 
         cards[5].style.backgroundColor = '#CD5C5C';
     }else cards[5].style.backgroundColor = '#90EE90';
 
     let r7 = document.getElementsByName("r7");
 
-    if (getValueFromRadio(r7) === "0") {
+    if (getValueFromRadio(r7) === "1") {
 
         arr_of_k[0] += 0.0;
-        arr_of_k[1] += 0.2;
-        arr_of_k[2] += 0.0;
+        arr_of_k[1] += 0.0;
+        arr_of_k[2] += 0.1;
         arr_of_k[3] += 0.1;
-        arr_of_k[4] += 0.0;
+        arr_of_k[4] += 0.4;
 
         cards[6].style.backgroundColor = '#CD5C5C';
     }else cards[6].style.backgroundColor = '#90EE90';
